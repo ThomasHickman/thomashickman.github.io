eb47a3753f5e6d56128a0e538099595e85ac7aee
Thomas Hickman <Thomas.Hickman42@gmail.com>
2019-03-30 22:23:07 +0000

    Remove autoreload css tag filtering

--- misc/cssrefresh2.js
+++ misc/cssrefresh2.js
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-Array.from(document.querySelectorAll("link[rel=stylesheet][autoreload]")).map(x => {
+Array.from(document.querySelectorAll("link[rel=stylesheet]")).map(x => {
     let lastModified = 0;
     async function poll(){
         let request = await fetch(`${x.href}?${Math.random()}`, {
